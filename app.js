893be69049b28dc9502ee87edf334f760faebf01
Artur555999 <142072644+Artur555999@users.noreply.github.com>
2023-10-14 12:53:04 +0300

    JS11

---
+++ app.js
@@ -0,0 +1,108 @@
+
+let item = document.querySelector('.item'); 
+let body = document.body;
+let item2 = document.querySelector('.item2')
+let item3 = document.querySelector('.item3')
+let item4 = document.querySelector('.item4')
+let item5 = document.querySelector('.item5')
+
+item.addEventListener('click',() => {
+     if(item = 'red') {
+    body.style.background = 'red'
+    }
+});
+item2.addEventListener('click', () => {
+  if (item2 ='yellow') {
+    body.style.background = 'yellow'
+  }
+}) 
+item3.addEventListener('click', () => {
+  if (item3 ='blue') {
+    body.style.background = 'blue'
+  }
+})
+item4.addEventListener('click', () => {
+  if (item4 ='black') {
+    body.style.background = 'black'
+  }
+})
+item5.addEventListener('click', () => {
+  if (item5 ='white') {
+    body.style.background = ''
+  }
+});   
+
+ let element1 = document.querySelector('.element1'),
+ element2 = document.querySelector('.element2'),
+ element3 = document.querySelector('.element3'),
+element4 = document.querySelector('.element4'),
+  element5 = document.querySelector('.element5'),
+ element6 = document.querySelector('.element6'),
+ element7 = document.querySelector('.element7'),
+element8 = document.querySelector('.element8')
+let el = document.querySelector('.el');
+let el2 = document.querySelector('.el2');
+let el3 = document.querySelector('.el3');
+let el4 = document.querySelector('.el4');
+let el5 = document.querySelector('.el5');
+
+el.addEventListener('click', () => {
+  if (el ='purple') {
+    element1.style.background = 'purple'
+    element2.style.background = 'purple'
+    element3.style.background = 'purple'
+    element4.style.background = 'purple'
+    element5.style.background = 'purple'
+    element6.style.background = 'purple'
+    element7.style.background = 'purple'
+    element8.style.background = 'purple'
+  }
+}); 
+el2.addEventListener('click', () => {
+  if (el2 ='lime') {
+    element1.style.background = 'lime'
+    element2.style.background = 'lime'
+    element3.style.background = 'lime'
+    element4.style.background = 'lime'
+    element5.style.background = 'lime'
+    element6.style.background = 'lime'
+    element7.style.background = 'lime'
+    element8.style.background = 'lime'
+  }
+}); 
+el3.addEventListener('click', () => {
+  if (el3 ='gray') {
+    element1.style.background = 'gray'
+    element2.style.background = 'gray'
+    element3.style.background = 'gray'
+    element4.style.background = 'gray'
+    element5.style.background = 'gray'
+    element6.style.background = 'gray'
+    element7.style.background = 'gray'
+    element8.style.background = 'gray'
+  }
+}); 
+el4.addEventListener('click', () => {
+  if (el4 ='aqua') {
+    element1.style.background = 'aqua'
+    element2.style.background = 'aqua'
+    element3.style.background = 'aqua'
+    element4.style.background = 'aqua'
+    element5.style.background = 'aqua'
+    element6.style.background = 'aqua'
+    element7.style.background = 'aqua'
+    element8.style.background = 'aqua'
+  }
+}); 
+el5.addEventListener('click', () => {
+  if (el5 ='white') {
+    element1.style.background = ''
+    element2.style.background = ''
+    element3.style.background = ''
+    element4.style.background = ''
+    element5.style.background = ''
+    element6.style.background = ''
+    element7.style.background = ''
+    element8.style.background = ''
+  }
+}); 
